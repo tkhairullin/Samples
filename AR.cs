57ea2e5449967be3871f82a02ede926b03cc249f
tkhairullin <t.a.khairullin@gmail.com>
2020-05-13 17:50:44 +0600

    Simple no SDK markerless AR with minimalistic UI (android)

---
+++ AR.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class AR : MonoBehaviour
+{
+    public RawImage display;
+	public RawImage displayReversed;
+	private WebCamTexture mCamera;
+	WebCamDevice[] devices;
+	public ButtonsManager BM;
+	public Quaternion baseRotation;
+
+	void Start()
+	{
+		devices = WebCamTexture.devices;
+		mCamera = new WebCamTexture();
+		mCamera.deviceName = devices[0].name;
+
+		display.texture = mCamera;
+		baseRotation = display.transform.rotation;
+		mCamera.Play();
+	}
+
+	void Update()
+    {
+		if (Input.deviceOrientation == DeviceOrientation.Portrait || Input.deviceOrientation == DeviceOrientation.PortraitUpsideDown)
+		{
+			display.transform.rotation = baseRotation * Quaternion.AngleAxis(mCamera.videoRotationAngle - 90, Vector3.back);
+		}
+
+		if (BM.flipper)
+        {
+			StartCoroutine(CamChange());
+		}
+	}
+
+	IEnumerator CamChange()
+    {
+		if (devices.Length > 1)
+		{
+			mCamera.Stop();
+			if (mCamera.deviceName == devices[0].name)
+			{
+				mCamera.deviceName = devices[1].name;
+			}
+			else
+			{
+				mCamera.deviceName = devices[0].name;
+			}
+
+			mCamera.Play();
+		}
+		BM.flipper = false;
+		yield return null;
+	}
+}
---
+++ ButtonsManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class ButtonsManager : MonoBehaviour
+{
+    public Button Reset;
+    public Button FlipCam;
+    public Button Exit;
+
+    public Transform Model;
+
+    public bool flipper=false;
+
+    public void ResetModel()
+    {
+        StartCoroutine(LerpAngle(1));
+    }
+
+    IEnumerator LerpAngle(float duration)
+    {
+        Model.DORotate(new Vector3(10, -160, 5), duration);
+        yield return 0;
+    }
+
+    public void FlipCamera()
+    {
+        flipper = true;
+    }
+
+    public void QuitApplication()
+    {
+        Application.Quit();
+    }
+}
